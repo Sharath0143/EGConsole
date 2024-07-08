88a263eafb17058d6e7dfc8817b00214f6d94d16
Sharath0143 <53374134+Sharath0143@users.noreply.github.com>
2024-07-09 01:37:22 +0530

    Add files via upload

---
+++ EGBuy/DashboardMenu.cs
@@ -0,0 +1,50 @@
+﻿using EGBuyBLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EGBuy
+{
+    public class DashboardMenu
+    {
+        public static void LoadCategories()
+        {
+            var Categories=CategoryManagement.GetCategories();
+            if(Categories.Count > 0 )
+            {
+                for(int i=0;i<Categories.Count; i++)
+                {
+                    var category = Categories[i];
+                    Console.WriteLine(category.CategoryId+ " - "+category.Name);
+                }
+                Console.WriteLine("Select your choice and hit enter");
+                int Choice = 0;
+                int.TryParse(Console.ReadLine(), out Choice);
+                if (Choice>0&&Choice<=Categories.Count)
+                {
+                    LoadProducts(Choice);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid selection. Please try again.");
+                }
+            }
+        }
+        public static void LoadProducts(int Choice)
+        {
+            var Products = ProductManagement.GetProducts(Choice);
+            for (int i = 0; i < Products.Count; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("ProductNumber {0}{1}ProductName {2}{3}Description{4}{5}Price {6}", Products[i].ProductId,Environment.NewLine, Products[i].Name,Environment.NewLine, Products[i].Description,Environment.NewLine, Products[i].Price);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("-----------------------------------------------------------------------------------------");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ EGBuy/Program.cs
@@ -0,0 +1,113 @@
+﻿using EGBuyBLL;
+using EGBuyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EGBuy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadDashboard();
+            LoadDeals();
+            LoadMenu();
+            Console.ReadKey();
+        }
+        private static void LoadDashboard()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("-----------------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                         EGBUY                                           ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("-----------------------------------------------------------------------------------------");
+        }
+        private static void LoadDeals()
+        {
+            try
+            {
+                List<Product> ProductList = ProductManagement.GetProductListByDiscount("DEALS");
+                if (ProductList.Count > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("*** DEALS FOR TODAY ***  ");
+                    Console.ResetColor();
+                    Console.WriteLine("Use coupon code DEAL20 to get flat 20% discount on below products");
+                    Console.WriteLine(Environment.NewLine);
+                    foreach (Product product in ProductList)
+                    {
+                        Console.Write("\t"+product.Name+"\t");
+                    }
+                    Console.WriteLine(Environment.NewLine);
+                }
+                else
+                {
+                    //Top deals bool  variable on and off
+                }
+            }
+            catch(Exception ex)
+            {
+                //Do nothing. No Product will be loading
+            }
+        }
+
+        private static void LoadMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("-----------------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Red;
+            //Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("MENU                                           ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("-----------------------------------------------------------------------------------------");
+            List<Menu> MenuList = MenuManagement.GetMenuItems();
+            if (MenuList.Count > 0)
+            {
+                foreach (Menu menu in MenuList)
+                {
+                    Console.WriteLine("{0} - {1}",menu.MenuId,menu.MenuName);
+                }
+                Console.WriteLine("Select your choice and hit enter");
+                int Choice = 0;
+                int.TryParse(Console.ReadLine(),out Choice);
+                if(Choice > 0 && Choice <= MenuList.Count)
+                {
+                    Console.Clear();
+                    LoadDashboard();
+                    switch (Choice)
+                    {
+                        case 1:
+                            DashboardMenu.LoadCategories();
+                            break;
+                        case 2:
+                            DashboardMenu.LoadCategories();
+                            break;
+                        case 3:
+                            DashboardMenu.LoadCategories();
+                            break;
+                        case 4:
+                            DashboardMenu.LoadCategories();
+                            break;
+                        default:
+                            DashboardMenu.LoadCategories();
+                            break;
+                    }       
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid selection. Please try again.");
+                    LoadMenu();
+                }
+            }
+        }
+    }
+}
